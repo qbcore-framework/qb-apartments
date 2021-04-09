7088ecff2890c747e70a2a61a08635de6a9b4b86
GhzGarage <qbcore@programmer.net>
2021-04-08 23:04:25 -0500

    Wait

--- client/main.lua
+++ client/main.lua
@@ -18,7 +18,7 @@ local rangDoorbell = nil
 Citizen.CreateThread(function()
 	while QBCore == nil do
 		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-		Citizen.Wait(0)
+		Citizen.Wait(200)
 	end
 end)
 
