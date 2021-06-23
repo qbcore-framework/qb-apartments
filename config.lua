aeeb6b9789166c38bd3fdc499a9409772b890c49
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-06-23 11:52:52 +0200

    Fix doorbell locations

--- config.lua
+++ config.lua
@@ -16,7 +16,7 @@ Apartments.Locations = {
         label = "Morningwood Blvd",
         coords = {
             enter = vector4(-1288.046, -430.126, 35.077, 305.348),
-            doorbell = vector4(-667.682, -1105.876, 14.629, 65.033),
+            doorbell = vector4(-1289.58, -429.59, 35.077, 305.348),
         }
     },
     ["apartment3"] = {
@@ -24,7 +24,7 @@ Apartments.Locations = {
         label = "Integrity Way",
         coords = {
             enter = vector4(269.075, -640.672, 42.02, 70.01),
-            doorbell = vector4(-667.682, -1105.876, 14.629, 65.033),
+            doorbell = vector4(270.33, -640.21, 42.02, 70.01),
         }
     },
     ["apartment4"] = {
@@ -32,7 +32,7 @@ Apartments.Locations = {
         label = "Tinsel Towers",
         coords = {
             enter = vector4(-621.016, 46.677, 43.591, 179.36),
-            doorbell = vector4(-667.682, -1105.876, 14.629, 65.033),
+            doorbell = vector4(-622.31, 47.00, 43.591, 179.36),
         }
     },
     ["apartment5"] = {
@@ -40,7 +40,7 @@ Apartments.Locations = {
         label = "Fantastic Plaza",
         coords = {
             enter = vector4(291.517, -1078.674, 29.405, 270.75),
-            doorbell = vector4(-667.682, -1105.876, 14.629, 65.033),
+            doorbell = vector4(291.16, -1077.06, 29.405, 270.75),
         }
     },
 }
