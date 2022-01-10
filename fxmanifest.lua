c43182b21c73ca5340851e14bc8d1fe76775e300
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-01-10 23:35:40 +1030

    oxmysql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,10 @@ version '1.0.0'
 
 shared_script 'config.lua'
 
-server_script 'server/main.lua'
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua'
+}
 
 client_scripts {
 	'client/main.lua',
