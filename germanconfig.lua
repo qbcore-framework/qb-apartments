e44484e1383207be88e06311ffad503bb9aa536e
Marius Gawronski <92158955+Seraph1982@users.noreply.github.com>
2022-01-20 13:31:39 +0100

    Create germanconfig.lua

---
+++ germanconfig.lua
@@ -0,0 +1,61 @@
+Apartments = {}
+
+Apartments.Starting = true
+
+Apartments.SpawnOffset = 30
+
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "South Rockford Drive",
+        coords = {
+            enter = vector4(-667.372, -1106.034, 14.629, 65.033),
+        }
+    },
+    ["apartment2"] = {
+        name = "apartment2",
+        label = "Morningwood Blvd",
+        coords = {
+            enter = vector4(-1288.046, -430.126, 35.077, 305.348),
+        }
+    },
+    ["apartment3"] = {
+        name = "apartment3",
+        label = "Integrity Way",
+        coords = {
+            enter = vector4(269.075, -640.672, 42.02, 70.01),
+        }
+    },
+    ["apartment4"] = {
+        name = "apartment4",
+        label = "Tinsel Towers",
+        coords = {
+            enter = vector4(-621.016, 46.677, 43.591, 179.36),
+        }
+    },
+    ["apartment5"] = {
+        name = "apartment5",
+        label = "Fantastic Plaza",
+        coords = {
+            enter = vector4(291.517, -1078.674, 29.405, 270.75),
+        }
+    },
+}
+
+Apartments.Language = {
+    ['enter'] = 'Gehe ins Aparment',
+    ['ring_doorbell'] = 'Betätige Türklingel',
+    ['logout'] = 'Logge Charakter aus',
+    ['change_outfit'] = 'Ändere Outfit',
+    ['open_stash'] = 'Öffne Fach',
+    ['move_here'] = 'Hierher bewegen',
+    ['open_door'] = 'Öffne Türe',
+    ['leave'] = 'Verlasse Apartment',
+    ['at_the_door'] = 'Jemand ist an der Tür!',
+    ['to_far_from_door'] = 'Du bist zu weit von der Türklingel entfernt',
+    ['close_menu'] = "⬅ Schließe Menu",
+    ['tennants'] = 'Mieter',
+    ['nobody_home'] = 'Es ist niemand zu Hause..',
+    ['receive_apart'] = "Du erhälst ein Apartment",
+    ['changed_apart'] = "Du bist mit dem Apartment umgezogen"
+}
