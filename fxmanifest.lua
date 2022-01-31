b0d070fe4d41cd948f90b4d53939818c0ab557f9
ThunderScripts <52550137+ThunderScripts@users.noreply.github.com>
2022-01-31 21:04:47 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,11 @@ game 'gta5'
 description 'QB-Apartments'
 version '1.0.0'
 
-shared_script 'config.lua'
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua', -- Change to the language you want
+}
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
