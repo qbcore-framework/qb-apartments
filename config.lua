cfa07fc3b1e458701d823df90f036c126ba2ced0
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2022-02-02 17:06:37 +0100

    Not needed when we switch to locale system

--- config.lua
+++ config.lua
@@ -41,21 +41,3 @@ Apartments.Locations = {
         }
     },
 }
-
-Apartments.Language = {
-    ['enter'] = 'Enter Apartment',
-    ['ring_doorbell'] = 'Ring Doorbell',
-    ['logout'] = 'Logout Character',
-    ['change_outfit'] = 'Change Outfit',
-    ['open_stash'] = 'Open Stash',
-    ['move_here'] = 'Move Here',
-    ['open_door'] = 'Open Door',
-    ['leave'] = 'Leave Apartment',
-    ['at_the_door'] = 'Someone is at the door!',
-    ['to_far_from_door'] = 'You are to far away from the Doorbell',
-    ['close_menu'] = "⬅ Close Menu",
-    ['tennants'] = 'Tennants',
-    ['nobody_home'] = 'There is nobody home..',
-    ['receive_apart'] = "You got a apartment",
-    ['changed_apart'] = "You moved apartments"
-}
