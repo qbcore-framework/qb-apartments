def39be20a072fc0286ab6a4c003a76423816bd5
Matti <57048210+MattiVboiii@users.noreply.github.com>
2023-02-20 19:35:09 +0100

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -2,6 +2,7 @@ local Translations = {
     error = {
         to_far_from_door = 'Je bent te ver van de deurbel',
         nobody_home = 'Er is niemand thuis..',
+        nobody_at_door = 'Er is niemand aan de deur...'
     },
     success = {
         receive_apart = 'Je hebt een appartement',
@@ -11,6 +12,7 @@ local Translations = {
         at_the_door = 'Er staat iemand voor de deur!',
     },
     text = {
+        options = '[E] Apartement Opties',
         enter = 'Betreed appartement',
         ring_doorbell = 'Aanbellen',
         logout = 'Karakter Uitloggen',
